634f6ee9ff51dac2cfa36d3a8a8afddfc99a4426
Keegan Pereira <keeganpereira222@gmail.com>
2022-06-14 11:12:40 +0100

    total for A, B and C

--- index.js
+++ index.js
@@ -19,8 +19,9 @@ console.log('hello')
 
 function checkoout(items){
 
-    let occurance = {}
+    let occurance = { }
 
+    //find the occurance of items
     for(const item of items){
         //if item not in occurance
         if(!(item in occurance)){
@@ -29,10 +30,33 @@ function checkoout(items){
             occurance[item] ++
         }
     }
-    console.log(occurance)
+    console.log(occurance.C)
+    let aTotal = naNtoZero((occurance.A * 50)/100)
+    let bTotal = 0
+    let cTotal = naNtoZero((occurance.C * 25)/100)
+    
+    //if item B divisible by 2
+    if(occurance.B % 2 === 0){
+        bTotal = occurance.B * 0.625
+    }
+    
+    //if item C divisible by 3
+    if(occurance.C % 3 === 0) {
+        console.log("free item C")
+    }
+
+    const total = parseFloat(aTotal + bTotal + cTotal).toFixed(2)
+
+    console.log(total)
 }
 
 
+function naNtoZero(val){
+    if(isNaN(val)){
+        return 0
+    }else{
+        return val
+    }
+}
 
-
-checkoout(['C', 'C', 'C', 'C'])
+checkoout(['C', 'C', 'C'])
