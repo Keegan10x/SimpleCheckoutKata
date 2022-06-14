ed529b057627068012ed0beb5d48c47ecc406b10
Keegan Pereira <keeganpereira222@gmail.com>
2022-06-14 12:20:37 +0100

    toggle discount & show basket

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 /* Simple Checkout Kata
 
 -Take in array of items A,B,C
@@ -8,61 +6,65 @@
 
 */
 
+console.log("Checkout Kata");
 
-
-
-
-
-console.log('hello')
-
-
-
-function checkoout(items){
-
-    let occurance = { }
-
-    //find the occurance of items
-    for(const item of items){
-        //if item not in occurance
-        if(!(item in occurance)){
-            occurance[item] = 1
-        }else{
-            occurance[item] ++
-        }
+function checkoout(items, applyDiscount) {
+  let occurance = {};
+  //find the occurance of items
+  for (const item of items) {
+    //if item not in occurance
+    if (!(item in occurance)) {
+      occurance[item] = 1;
+    } else {
+      occurance[item]++;
     }
-    console.log(occurance.C)
-    let aTotal = naNtoZero((occurance.A * 50)/100)
-    let bTotal = 0
-    let cTotal = naNtoZero((occurance.C * 25)/100)
-    
+  }
+
+  if (!applyDiscount) {
+    let aTotal = naNtoZero((occurance.A * 50) / 100);
+    let bTotal = naNtoZero((occurance.A * 75) / 100);
+    let cTotal = naNtoZero((occurance.C * 25) / 100);
+    const total = aTotal + bTotal + cTotal;
+    console.log(occurance);
+    console.log("Fee in £: " + total);
+  } else if (applyDiscount) {
+    let aTotal = naNtoZero((occurance.A * 50) / 100);
+    let bTotal = 0;
+    let cTotal = naNtoZero((occurance.C * 25) / 100);
+
     //if item B divisible by 2
-    if(occurance.B % 2 === 0){
-        bTotal = occurance.B * 0.625
+    if (occurance.B % 2 === 0) {
+      bTotal = occurance.B * 0.625;
     }
-    if(occurance.B % 2 === 1){
-        bTotal = Math.floor(occurance.B / 2) * 2 * 0.625
-        bTotal += 0.75
+    if (occurance.B % 2 === 1) {
+      bTotal = Math.floor(occurance.B / 2) * 2 * 0.625;
+      bTotal += 0.75;
     }
-    
+
     //if item C divisible by 3
-    //if(occurance.C % 3 === 0) {
-    //    console.log("free item C")
+    const dealDisplay = occurance;
+    if (dealDisplay.C != undefined) {
+      dealDisplay.C += Math.floor(occurance.C / 3);
+    }
+
+    //if(dealDisplay.C % 3 === 0) {
+    //    dealDisplay.C += Math.floor(occurance.B / 3)
+    //}else{
+    //   dealDisplay.C += Math.floor(occurance.B / 3)
     //}
 
-    
-
-    const total = parseFloat(aTotal + bTotal + cTotal).toFixed(2)
-
-    console.log(total)
+    const total = parseFloat(aTotal + bTotal + cTotal).toFixed(2);
+    console.log(dealDisplay);
+    console.log("Fee in £: " + total);
+  }
 }
 
-
-function naNtoZero(val){
-    if(isNaN(val)){
-        return 0
-    }else{
-        return val
-    }
+function naNtoZero(val) {
+  if (isNaN(val)) {
+    return 0;
+  } else {
+    return val;
+  }
 }
 
-checkoout(['B', 'B', 'C', 'C', 'C'])
+checkoout(["B", "B", "C", "C", "C"], true);
