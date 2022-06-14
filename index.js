c6bde97a00a4d7272faf52b511935e3b317090a7
Keegan Pereira <keeganpereira222@gmail.com>
2022-06-14 10:07:56 +0100

    find item occurance

--- index.js
+++ index.js
@@ -18,5 +18,21 @@ console.log('hello')
 
 
 function checkoout(items){
-    
+
+    let occurance = {}
+
+    for(const item of items){
+        //if item not in occurance
+        if(!(item in occurance)){
+            occurance[item] = 1
+        }else{
+            occurance[item] ++
+        }
+    }
+    console.log(occurance)
 }
+
+
+
+
+checkoout(['C', 'C', 'C', 'C'])
